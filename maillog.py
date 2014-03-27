fa61eae6784061ec0103b57cff782094330773b4
Renato <renato@renato-laptop.(none)>
2014-03-27 00:14:17 -0300

    Added support for attaching files in e-mail

--- maillog.py
+++ maillog.py
@@ -7,7 +7,7 @@ terminal or from other Python programs using an external SMTP e-mail account.
 For information on how to install, see the README file.
 
 Author: Renato Candido <renato@liria.com.br>
-Copyright 2013 Liria Tecnologia <http://www.liria.com.br>
+Copyright 2014 Liria Tecnologia <http://www.liria.com.br>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,6 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Changelog:
 
+2014-03-27
+Added support for attaching files in e-mail
+
 2013-06-13
 Fixed to work with multiline messages.
 
@@ -34,9 +37,14 @@ Added support for SMTP using SSL and multiple recipients.
 Initial commit.
 """
 
-from textwrap import dedent
 import sys
+import os
 from smtplib import SMTP, SMTP_SSL
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
 
 # External SMTP account configuration
 SENDER_NAME = "Maillog e-mail"
@@ -48,39 +56,57 @@ SMTP_PASSWORD = "smtp_password"
 USE_SSL = False
 # End of configuration
 
-def send_mail(destinations, subject, message, sender_name=SENDER_NAME,
+def send_mail(destinations, subject, message, files=[], sender_name=SENDER_NAME,
               sender_email=SENDER_EMAIL, smtp_server=SMTP_SERVER,
               smtp_port=SMTP_PORT, smtp_login=SMTP_LOGIN,
               smtp_password=SMTP_PASSWORD, use_ssl=USE_SSL):
     """
-    Sends an e-mail to the destinations provided, with the subject and message
-    provided. Optionally, the external SMTP account configuration can be set up
-    through arguments on the function call.
+    Sends an e-mail to the destinations provided, with the subject, message and
+    attached files provided. Optionally, the external SMTP account configuration
+    can be set up through arguments on the function call.
     """
-    destinations_list = destinations.split(';')
 
-    for destination in destinations_list:
-        destination = destination.strip()
-        email =  dedent("""\
-        From: %s <%s>
-        To: %s
-        Subject: %s
-        
-        """ % (sender_name, sender_email, destination, subject))
+    if type(destinations) is str:
+        destinations = destinations.split(';')
+        for n, destination in enumerate(destinations):
+            destinations[n] = destination.strip()
 
-        email = email + message
-     
+    if type(files) is str:
+        files = files.split(';')
+        for n, file in enumerate(files):
+            files[n] = file.strip()
+
+    email = MIMEMultipart()
+    email['From'] = sender_name
+    email['To'] = COMMASPACE.join(destinations)
+    email['Date'] = formatdate(localtime=True)
+    email['Subject'] = subject
+    email.attach(MIMEText(message))
+    
+    for f in files:
+        part = MIMEBase('application', "octet-stream")
         try:
-            if use_ssl:
-                smtpObj = SMTP_SSL(smtp_server, smtp_port)
-            else:
-                smtpObj = SMTP(smtp_server, smtp_port)
-            smtpObj.login(smtp_login, smtp_password)
-            smtpObj.sendmail(sender_email, destination, email)
-            print "Successfully sent email"
+            part.set_payload(open(f,"rb").read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition',
+                        'attachment; filename="%s"' % os.path.basename(f))
+            email.attach(part)
         except:
-            print "Error: unable to send email"
+            print "File " + f + " not found"
             sys.exit(1)
+     
+    try:
+        if use_ssl:
+            smtpObj = SMTP_SSL(smtp_server, smtp_port)
+        else:
+            smtpObj = SMTP(smtp_server, smtp_port)
+        smtpObj.login(smtp_login, smtp_password)
+        smtpObj.sendmail(sender_email, destinations, email.as_string())
+        if __name__ == '__main__':
+            print "Successfully sent email"
+    except:
+        print "Error: unable to send email"
+        sys.exit(1)
 
 def main():
     """
@@ -88,21 +114,32 @@ def main():
     send_mail() function. This function expects the script to be run as
     maillog.py "<destination email addresses>" "<subject>" "<message>"
     """
-    if len(sys.argv) != 4:
+    if (len(sys.argv) < 4 or len(sys.argv) > 5 or sys.argv[1] == '-h'
+        or sys.argv[1] == '--help'):
         print ("Usage " + __file__ + " \"<destination email addresses>\" "
-               + "\"<subject>\"" + " \"<message>\"")
+               + "\"<subject>\"" + " \"<message>\"" + " \"[<files>]\"")
         print ("Example: " + __file__ + " \"bob@gmail.com; ann@gmail.com\" " 
                + "\"Maillog test\"" + " \"If you received this, it worked!\"")
         print ""
-        print ("In case of a single recipient, the first argument may not"
-               + " need quotes. For example:\n")
+        print ("In case of a single recipient, the first argument might not"
+               + " need quotes (in case of no spaces, in general)."
+               + " For example:\n")
         print (__file__ + " bob@gmail.com " 
                + "\"Maillog test\"" + " \"If you received this, it worked!\"")
+        print ""
+        print ("Optionally, files can be attached to the e-mailI. For example:\n")
+        print (__file__ + " \"bob@gmail.com; ann@gmail.com\" " 
+               + "\"Maillog test\"" + " \"If you received this, it worked!\""
+               + "./dir1/file1; ./dir2/file2")
     else:
         destinations = sys.argv[1]
         subject = sys.argv[2]
         message = sys.argv[3]
-        send_mail(destinations, subject, message)
+        if len(sys.argv) == 5:
+            files = sys.argv[4]
+            send_mail(destinations, subject, message, files)
+        else:
+            send_mail(destinations, subject, message)
 
 if __name__ == '__main__':
     main()
